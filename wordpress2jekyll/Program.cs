191f48b4bf5056e41ca784d1840b6a498794fd7f
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2019-10-16 15:44:26 -0400

    Finding xml files

--- wordpress2jekyll/Program.cs
+++ wordpress2jekyll/Program.cs
@@ -1,4 +1,8 @@
 ﻿using System;
+using System.IO;
+using System.IO.Compression;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace wordpress2jekyll
 {
@@ -6,7 +10,29 @@ namespace wordpress2jekyll
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            ImportAsync("export.zip").Wait();
+        }
+
+        private static async Task ImportAsync(string zipFilePath)
+        {
+            try
+            {
+                using (var stream = File.OpenRead(zipFilePath))
+                using (var archive = new ZipArchive(stream, ZipArchiveMode.Read))
+                {
+                    var xmlEntries = from e in archive.Entries
+                                     where e.Length != 0
+                                     && e.Name.EndsWith(".xml")
+                                     orderby e.FullName
+                                     select e;
+
+                    throw new NotImplementedException();
+                }
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.Error.WriteLine($"File '{ex.FileName}' not found");
+            }
         }
     }
 }
