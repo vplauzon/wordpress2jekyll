5f946f27ad9c8df52e9d29b1e0a241f6d8a5623b
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2020-01-02 08:39:39 -0500

    Remove confidentials about authors

---
+++ wordpress2jekyll/Author.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace wordpress2jekyll
+{
+    internal class Author
+    {
+        public Author(
+            string name,
+            string url)
+        {
+            Name = name;
+            Url = url;
+        }
+
+        public string Name { get; }
+
+        public string Url { get; }
+    }
+}
--- wordpress2jekyll/Comment.cs
+++ wordpress2jekyll/Comment.cs
@@ -16,18 +16,13 @@ namespace wordpress2jekyll
             var comments = from e in elements
                            let id = e.Element(Post.WP + "comment_id").Value
                            let author = e.Element(Post.WP + "comment_author").Value
-                           let authorIp = e.Element(Post.WP + "comment_author_IP").Value
-                           let authorEmail = e.Element(Post.WP + "comment_author_email").Value
                            let authorUrl = e.Element(Post.WP + "comment_author_url").Value
                            let content = e.Element(Post.WP + "comment_content").Value
                            let dateGmt = e.Element(Post.WP + "comment_date_gmt").Value
                            let date = DateTime.Parse(dateGmt, null, DateTimeStyles.AssumeUniversal)
                            select new Comment(
                                id,
-                               author,
-                               authorIp,
-                               authorEmail,
-                               authorUrl,
+                               new Author(author, authorUrl),
                                date,
                                content);
 
@@ -36,31 +31,19 @@ namespace wordpress2jekyll
 
         private Comment(
             string id,
-            string author,
-            string authorIp,
-            string authorEmail,
-            string authorUrl,
+            Author author,
             DateTime date,
             string content)
         {
             Id = id;
             Author = author;
-            AuthorIp = authorIp;
-            AuthorEmail = authorEmail;
-            AuthorUrl = authorUrl;
             Date = date;
             Content = content;
         }
 
         public string Id { get; }
 
-        public string Author { get; }
-
-        public string AuthorIp { get; }
-
-        public string AuthorEmail { get; }
-
-        public string AuthorUrl { get; }
+        public Author Author { get; }
 
         public DateTime Date { get; }
 
