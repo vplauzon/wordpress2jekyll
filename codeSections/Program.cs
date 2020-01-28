e6276c6198619fe97b4f47540c110d68fc2280b8
Vincent-Philippe Lauzon <vincent_lauzon@yahoo.com.au>
2020-01-27 21:16:33 -0500

    Replace code

--- codeSections/Program.cs
+++ codeSections/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Immutable;
 using System.IO;
 using System.Linq;
+using System.Net;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 
@@ -24,7 +26,6 @@ namespace codeSections
 
                 foreach (var path in filePaths)
                 {
-                    Console.WriteLine($"Path:  {path}");
                     await ProcessPostAsync(path);
                 }
             }
@@ -36,12 +37,73 @@ namespace codeSections
             var beginRegexText = GetResource("begin-code-regex.txt");
             var beginRegex = new Regex(beginRegexText);
             var endRegexText = GetResource("end-code-regex.txt");
-            var endRegex = new Regex(beginRegexText);
+            var endRegex = new Regex(endRegexText);
             var beginMatch = beginRegex.Match(content);
             var endMatch = endRegex.Match(content);
 
             if (beginMatch.Success)
             {
+                Console.WriteLine($"Code found in path '{path}'");
+
+                ValidateMatches(beginMatch, endMatch);
+
+                var newContentParts = ReplaceCode(content, 0, beginMatch, endMatch);
+                var newContent = string.Concat(newContentParts);
+            }
+        }
+
+        private static IImmutableList<string> ReplaceCode(
+            string content,
+            int indexStart,
+            Match beginMatch,
+            Match endMatch)
+        {
+            var top = content.Substring(indexStart, beginMatch.Index - indexStart);
+            var beginTag = "```" + beginMatch.Groups["lang"].Value + Environment.NewLine;
+            var blockEnd = endMatch.Index + endMatch.Value.Length;
+            var code = content.Substring(
+                beginMatch.Index + beginMatch.Value.Length,
+                endMatch.Index - beginMatch.Index - beginMatch.Value.Length);
+            var unencodedCode = WebUtility.HtmlDecode(code);
+            var endTag = "```";
+            var parts = ImmutableList<string>
+                .Empty
+                .Add(top)
+                .Add(beginTag)
+                .Add(unencodedCode)
+                .Add(endTag);
+
+            if (beginMatch.NextMatch().Success)
+            {
+                var nextParts = ReplaceCode(
+                    content,
+                    blockEnd,
+                    beginMatch.NextMatch(),
+                    endMatch.NextMatch());
+
+                return parts.AddRange(nextParts);
+            }
+            else
+            {
+                var remainer = content.Substring(blockEnd);
+
+                return parts.Add(remainer);
+            }
+        }
+
+        private static void ValidateMatches(Match beginMatch, Match endMatch)
+        {
+            if (beginMatch.Success != endMatch.Success)
+            {
+                throw new InvalidDataException("Different cardinality of begin & end of code");
+            }
+            if (beginMatch.Success)
+            {
+                if (beginMatch.Index >= endMatch.Index)
+                {
+                    throw new InvalidDataException("Begin code happens after the end");
+                }
+                ValidateMatches(beginMatch.NextMatch(), endMatch.NextMatch());
             }
         }
 
